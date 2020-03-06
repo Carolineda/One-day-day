a43d0d21fbe4b35de5ca6eefda5f3ee9c1d796a9
大大大大黄呀 <835814273@qq.com>
2020-03-06 21:37:00 +0800

    commit file

--- 算法/04_重建二叉树/index.js
+++ 算法/04_重建二叉树/index.js
@@ -1,20 +1,25 @@
-/**
- * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
- * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
- * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，
- * 则重建二叉树并返回。
- */
+// /* function TreeNode(x) {
+//     this.val = x;
+//     this.left = null;
+//     this.right = null;
+// } */
 
 
- var listNode = function Node(val)
- {
-   this.val = val;
-   this.left = null;
-   this.right = null
- }
 
 
-function reConstructBinaryTree(pre, vin)
-{
-    
+function reConstructBinaryTree(pre, vin) {
+    // write code here
+    var head = new TreeNode(pre[0]),
+        headIndex = vin.indexOf(pre[0]),
+        leftVin = vin.slice(0, headIndex),
+        rightVin = vin.slice(headIndex + 1),
+        leftPre = pre.slice(1, 1 + leftVin.length),
+        rightPre = pre.slice(1 + leftVin.length);
+    if(leftPre.length != 0){
+        head.left = reConstructBinaryTree(leftPre, leftVin);
+    }
+    if(rightPre.length !=0 ){
+        head.right = reConstructBinaryTree(rightPre, rightVin);
+    }
+    return head;
 }
